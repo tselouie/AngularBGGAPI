804c31874a28ab485c1e8eef186302f6ca698ec6
Louie Tse <28274608+tselouie@users.noreply.github.com>
2020-03-03 22:06:00 -0500

    fix

--- src/app/data/boardgame.service.ts
+++ src/app/data/boardgame.service.ts
@@ -14,10 +14,9 @@ private url = "https://www.boardgamegeek.com/xmlapi2/hot?type=boardgame";
 
   constructor(private http: HttpClient) {}
   
-
   getBoardGames(): Observable<any[]> {
 
-      const jsonparser = require('xml2json');
+      const jsonparser = require('xml2js').parseString;
       var bgarray : Boardgame[];
 
      fetch(this.url)
@@ -28,22 +27,37 @@ private url = "https://www.boardgamegeek.com/xmlapi2/hot?type=boardgame";
           let xml = parser.parseFromString(data, "application/xml");
           console.log(xml);
           
-          let json = jsonparser.toJson(data);
-     
-          console.log(json);
+        // let json = jsonparser(data, function (err, result) {
+        //   console.log(result.items.item[0].yearpublished[0].$.value); //year of board game
+        //   console.log(result.items.item[0].name[0].$.value);//name of the board game
+        //     console.log(result.items.length());
+            
+           
+        // });
+            var len = xml.getElementsByTagName('item').length;
+            var items = xml.getElementsByTagName('item');
+
+            for( var i = 0;i< len;i++){  
+              var bg = new Boardgame();
+              bg.BoardGameName = items[i].children[1].getAttribute('value');
+              bg.Thumbnail = items[i].children[0].getAttribute('value');
+              bg._id = items[i].getAttribute('id');
+                bgarray.push(bg);
+            }
+
           
-          
-        })
+        //    xml.getElementsByTagName('item').forEach(element => {
+        //   var bg = new Boardgame
+        //   bg.BoardGameName = element.children[1]
+        //   bg.Thumbnail = element.children[0]
+        //   bg._id = element.children[2]
+        //   bgarray.push(bg)
+        // })
          
-          //  xml.getElementsByTagName('item').forEach(element => {
-          // var bg = new Boardgame
-          // bg.BoardGameName = element.children[1]
-          // bg.Thumbnail = element.children[0]
-          // bg._id = element.children[2]
-          // bgarray.push(bg)
           
-        // })
-        // })
+          
+         console.log(bgarray);
+         })
 
     return this.http.get<Boardgame[]>(`${this.url}/boardgames`)
        
